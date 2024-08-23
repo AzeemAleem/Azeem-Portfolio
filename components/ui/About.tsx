10bc6d24374de3693da4e15075f93ca39c9b1daf
Awais12xd <lucifer.awaisxx213@gmail.com>
2024-08-23 06:54:46 -0700

    Update About.tsx

--- components/ui/About.tsx
+++ components/ui/About.tsx
@@ -103,7 +103,7 @@ const About = () => {
                 viewport={{ once: true }}
                 variants={cardVariants}
               >
-                Thanks for visiting my profile. I'm a Microsoft Certified Solutions Associate. I have been working as a Senior Software Enginner for more than 5 years and have rich experience working on React js, Node Js, Next js, AWS, GraphQl, Relational and Non relation (MongoDB) database. <br /> <br /> I turn complex challenges into seamless digital solutions. With extensive experience to design scalable architecture, I am managing cloud infrastructure on AWS, ensuring seamless integration and scalability. I completed more than 50 web projects including Blockchain, Ecommerce, Accounts and Corporate portals etc.  I focused on delivering high-quality, maintainable code.
+                Thanks for visiting my profile. I am a Microsoft Certified Solutions Associate. I have been working as a Senior Software Enginner for more than 5 years and have rich experience working on React js, Node Js, Next js, AWS, GraphQl, Relational and Non relation (MongoDB) database. <br /> <br /> I turn complex challenges into seamless digital solutions. With extensive experience to design scalable architecture, I am managing cloud infrastructure on AWS, ensuring seamless integration and scalability. I completed more than 50 web projects including Blockchain, Ecommerce, Accounts and Corporate portals etc.  I focused on delivering high-quality, maintainable code.
                 <br /> <br />
                 <p className="text-center text-[#c7d1f0] text-lg mb-4">Technologies I’m Proficient In</p>
                 {stack.map((item) => (
@@ -121,21 +121,10 @@ const About = () => {
                   </div>
                 ))}
               </motion.div>
-              {/* <motion.p
-                initial="offscreen"
-                whileInView="onscreen"
-                viewport={{ once: true }}
-                variants={cardVariants}
-              >
-                Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at the Dev Corporate for a variety of clients.
-              </motion.p> */}
-              {/* <p>
-                Here are a few technologies I’ve been working with recently:
-              </p> */}
+            
+             
             </div>
-            {/* <div>
-              <Image src={about} alt='about' className='custom-img' />
-            </div> */}
+          
           </div>
         </div>
       </Container>
