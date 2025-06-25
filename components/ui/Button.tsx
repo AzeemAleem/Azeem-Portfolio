1ae6cccd76c14b79e5484bcfe2169980e1e1f470
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-25 23:41:45 +0500

    Update Button.tsx

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { ReactNode } from 'react';
 
-interface props {
-  text: string
-  link?: string
-  size?: string
-  hover?: boolean
+interface Props {
+  text?: string;
+  link?: string;
+  size?: 'small' | 'medium';
+  hover?: boolean;
+  children?: ReactNode;
 }
 
-const Button = ({ text, size = 'medium' }: props) => {
-  const smallStyle = 'h-10 px-4 text-[13px]'
-  const mediumStyle = 'h-12 px-8 md:text-base text-[13px] font-light'
+const Button = ({ text, size = 'medium', children }: Props) => {
+  const smallStyle = 'h-10 px-4 text-[13px]';
+  const mediumStyle = 'h-12 px-8 md:text-base text-[13px] font-light';
 
   return (
-    <button className={`bg-transparent text-primary border border-primary btn-hover-effect transition-all duration-150 ease-in rounded ${size === 'small' ? smallStyle : mediumStyle }`}>{text}</button>
-  )
-}
+    <button
+      className={`bg-transparent text-primary border border-primary btn-hover-effect transition-all duration-150 ease-in rounded ${size === 'small' ? smallStyle : mediumStyle}`}
+    >
+      {text}
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
